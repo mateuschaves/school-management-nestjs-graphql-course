8b17110373e98ab14fb3bc857856e2c6713f73b0
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-06-12 00:47:02 -0300

    Persistence: TypeORM, MongoDB and our Lesson Entity

---
+++ src/lesson/lesson.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryColumn, Column, ObjectIdColumn } from "typeorm";
+
+@Entity()
+export default class Lesson {
+
+    @ObjectIdColumn()
+    _id: string;
+
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    startDate: string;
+
+    @Column()
+    endDate: string;
+}
