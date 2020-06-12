e25f00d70f776092547e67359aa06a03b350b70b
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-06-12 00:54:15 -0300

    LessonService and createLesson method

--- src/lesson/lesson.module.ts
+++ src/lesson/lesson.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { LessonResolver } from './lesson.resolver';
+import { LessonService } from './lesson.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Lesson from '../lesson/lesson.entity';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([
+            Lesson
+        ])
+    ],
     providers: [
-        LessonResolver
+        LessonResolver,
+        LessonService
     ]
 })
 export class LessonModule { }
