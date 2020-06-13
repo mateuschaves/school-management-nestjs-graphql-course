9f4a2f3abf6fb8d28d4281633e9caa0418002ae4
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-06-13 13:52:03 -0300

    Challenge: Create Student Mutation

---
+++ src/student/student.type.ts
@@ -0,0 +1,14 @@
+import { Field, ObjectType, ID } from '@nestjs/graphql';
+
+@ObjectType('Student')
+export class StudentType {
+
+    @Field(type => ID)
+    id: string;
+
+    @Field()
+    firstName: string;
+
+    @Field()
+    lastName: string;
+}
